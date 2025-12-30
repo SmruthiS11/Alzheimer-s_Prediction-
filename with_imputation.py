83814d5c8ab940c1edb5d34c4b1c6bcd979523e5
SmruthiS11 <122665857+SmruthiS11@users.noreply.github.com>
2025-12-30 07:33:31 +0530

    Create with_imputation.py

---
+++ with_imputation.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.impute import SimpleImputer
+
+df = pd.read_csv("oasis_longitudinal.csv")
+
+print("Missing values before imputation:")
+print(df.isna().sum())
+
+# Define the column groups based on their data types.
+numeric_cols = ["Visit", "MR Delay", "Age", "EDUC", "SES", "MMSE", "CDR", "eTIV", "nWBV", "ASF"]
+categorical_cols = ["Subject ID", "MRI ID", "Group", "M/F", "Hand"]
+
+# Impute numeric columns by replacing missing values with the median
+num_imputer = SimpleImputer(strategy="mean")
+df[numeric_cols] = num_imputer.fit_transform(df[numeric_cols])
+
+# Impute categorical columns
+cat_imputer = SimpleImputer(strategy="constant", fill_value = "missing")
+df[categorical_cols] = cat_imputer.fit_transform(df[categorical_cols])
+
+print("\nMissing values after imputation:")
+print(df.isna().sum())
+
+# Saving the imputed dataset to a new CSV file.
+df.to_csv("oasis_longitudinal_imputed.csv", index=False)
+print("\nImputation complete. The imputed dataset has been saved to 'oasis_longitudinal_imputed.csv'.")
